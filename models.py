98d958b3bd9a389997dbd6d98708c339a19a1eba
akcsri <akihiko.kaneko@csri-japan.com>
2025-07-14 21:01:19 +0900

    Add files via upload

---
+++ models.py
@@ -0,0 +1,32 @@
+
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    participants = db.relationship('Participant', backref='user', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Participant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    position = db.Column(db.String(150), nullable=False)
+    questions = db.Column(db.Text, nullable=True)
+    agm_status = db.Column(db.String(50), nullable=True)
+    lpac_status = db.Column(db.String(50), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+def get_user_by_username(username):
+    return User.query.filter_by(username=username).first()
